021053f15bafbb7d90691950970d9f63c2b6e089
Helder S Ribeiro <git@agentofuser.com>
2020-03-24 02:38:26 -0400

    Updates

---
+++ precache-manifest.bce50648a5bad8d00dcae666ad411bdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c5464eef07c954034a241f6629233c",
+    "url": "/w00t/index.html"
+  },
+  {
+    "revision": "2dcb4f755d5ace95584d",
+    "url": "/w00t/static/css/main.a25bac32.chunk.css"
+  },
+  {
+    "revision": "378ac9125fcdd0340efd",
+    "url": "/w00t/static/js/2.e9ec884a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/w00t/static/js/2.e9ec884a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dcb4f755d5ace95584d",
+    "url": "/w00t/static/js/main.939b50e9.chunk.js"
+  },
+  {
+    "revision": "7e38af111c1c3cc61f47",
+    "url": "/w00t/static/js/runtime-main.5744fd98.js"
+  }
+]);
